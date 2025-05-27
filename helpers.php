5ca17348f2f729af39dd3e187d30e6fe442551dc
Aixufey <jack_11728@msn.com>
2025-05-27 20:20:09 +0200

    add helper fn to load view and partials

--- helpers.php
+++ helpers.php
@@ -1,5 +1,6 @@
 <?php
-declare(strict_types = 1);
+
+declare(strict_types=1);
 
 /**
  * Get the base path
@@ -7,6 +8,42 @@ declare(strict_types = 1);
  * @param string $path
  * @return string
  */
-function basePath(string $path = ''): string {
+function basePath(string $path = ''): string
+{
   return __DIR__ . '/' . $path;
+}
+
+
+/**
+ * Load a view from the base path
+ * E.g. loading a `home.view.php` script we can pass in the name `home` <p>
+ * </p>
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadView(string $name): void
+{
+  $viewPath = basePath("views/{$name}.view.php");
+
+  if (file_exists($viewPath)) {
+    require $viewPath;
+  } else {
+    echo "View '{$name}' not found!";
+  }
+}
+
+/**
+ * Load a partial that is reusable code from subfolder of views
+ * @param string $name
+ * @return void
+ */
+function loadPartial(string $name): void
+{
+  $partialPath = basePath("views/partials/{$name}.php");
+  if (file_exists($partialPath)) {
+    require $partialPath;
+  } else {
+    echo "View '{$name}' not found!";
+  }
 }
