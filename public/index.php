39cecc7a03374d972f4ac0e7ba5dc277a0c7d3d1
Aixufey <jack_11728@msn.com>
2025-05-30 19:11:56 +0200

    refactor Router class

--- public/index.php
+++ public/index.php
@@ -1,6 +1,16 @@
 <?php
 require '../helpers.php';
 
-$uri = $_SERVER['REQUEST_URI'];
+// Require Router script
+require basePath('Router.php');
+// Instantiate router
+$router = new Router();
+// Require available routes
+$routes = require basePath('routes.php');
 
-require basePath('router.php');
+// Request URI and method
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Pass request args into our router and route to correct controller.
+$router->route($uri, $method);
--- routes.php
+++ routes.php
@@ -1,9 +1,14 @@
 <?php
 
-// Routes
-return [
-  '/' => 'controllers/home.php',
-  '/listings' => 'controllers/listings/index.php',
-  '/listings/create' => 'controllers/listings/create.php',
-  '404' => 'controllers/error/404.php'
-];
+// // Routes
+// return [
+//   '/' => 'controllers/home.php',
+//   '/listings' => 'controllers/listings/index.php',
+//   '/listings/create' => 'controllers/listings/create.php',
+//   '404' => 'controllers/error/404.php'
+// ];
+
+// Router Class will register uri and controller requests, 404 is handled internally.
+$router->get('/', 'controllers/home.php');
+$router->get('/listings', 'controllers/listings/index.php');
+$router->get('/listings/create', 'controllers/listings/create.php');
